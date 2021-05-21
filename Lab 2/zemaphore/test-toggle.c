6e38af596adffcb576f54a9e8feaa8223d604125
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-22 01:03:04 +0530

    part2 complete

--- Lab 2/zemaphore/test-toggle.c
+++ Lab 2/zemaphore/test-toggle.c
@@ -12,45 +12,24 @@
 #define NUM_THREADS 3
 #define NUM_ITER 10
 
-zem_t z1, z2, z3;
+int Z = 0;
+
+zem_t zem[NUM_THREADS];
 
 void *justprint(void *data)
 {
   int thread_id = *((int *)data);
-  if (thread_id == 0)
-    zem_down(&z1);
-  else if (thread_id == 1)
-    zem_down(&z2);
-  else
-    zem_down(&z3);
+
+  zem_down(&zem[thread_id]);
+
   for (int i = 0; i < NUM_ITER; i++)
   {
     printf("This is thread %d\n", thread_id);
-    if (thread_id == 0 && i != NUM_ITER - 1)
+
+    zem_up(&zem[(thread_id + 1) % NUM_THREADS]);
+    if (i != NUM_ITER - 1)
     {
-      zem_up(&z2);
-      zem_down(&z1);
-    }
-    else if (thread_id == 0 && i == NUM_ITER - 1)
-    {
-      zem_up(&z2);
-    }
-    else if (thread_id == 1 && i != NUM_ITER - 1)
-    {
-      zem_up(&z3);
-      zem_down(&z2);
-    }
-    else if (thread_id == 1 && i == NUM_ITER - 1)
-    {
-      zem_up(&z3);
-    }
-    else if (thread_id == 2 && i != NUM_ITER - 1)
-    {
-      zem_up(&z1);
-      zem_down(&z3);
-    }
-    else
-    {
+      zem_down(&zem[thread_id]);
     }
   }
   return 0;
@@ -58,18 +37,24 @@ void *justprint(void *data)
 
 int main(int argc, char *argv[])
 {
-  zem_init(&z1, 0);
-  zem_init(&z2, 0);
-  zem_init(&z3, 0);
+
   pthread_t mythreads[NUM_THREADS];
+
   int mythread_id[NUM_THREADS];
+
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    zem_init(&zem[i], 0);
+  }
 
   for (int i = 0; i < NUM_THREADS; i++)
   {
     mythread_id[i] = i;
     pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
   }
-  zem_up(&z1);
+
+  zem_up(&zem[0]);
+
   for (int i = 0; i < NUM_THREADS; i++)
   {
     pthread_join(mythreads[i], NULL);
